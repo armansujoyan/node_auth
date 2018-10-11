bb3393e9f9ce3050c9e35af6928a85541bacf4dc
Arman Sujoyan <arman@brainbox.am>
2018-10-11 13:45:05 +0400

    Add hashing and slating of passwords

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const { Schema } = mongoose;
 
@@ -14,6 +15,24 @@ const UserSchema = new Schema({
     required: true,
   },
 });
+
+UserSchema.pre('save', async function passHash(next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(this.password, salt);
+    this.password = passwordHash;
+  } catch (error) {
+    next(error);
+  }
+});
+
+UserSchema.methods.isValidPassword = async function validatePass(newPassword) {
+  try {
+    return await bcrypt.compare(newPassword, this.password);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
 
 const User = mongoose.model('user', UserSchema);
 
